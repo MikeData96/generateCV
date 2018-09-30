cd10ce441cbf03a27fe20cab178ffdff7b8eaadd
danielblacha <daniel.blacha@gmail.com>
2018-09-30 14:12:54 +0200

    add model

---
+++ src/main/java/pl/akademiakodu/generateCv/controller/PersonController.java
@@ -0,0 +1,16 @@
+package pl.akademiakodu.generateCv.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import pl.akademiakodu.generateCv.model.Person;
+
+@Controller
+public class PersonController {
+
+    @GetMapping("/")
+    public String showForm(ModelMap modelMap){
+        modelMap.addAttribute("person", new Person());
+        return "homePage";
+    }
+}
