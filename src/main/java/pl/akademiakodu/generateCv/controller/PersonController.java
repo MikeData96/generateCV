58efff678033e3babf1386cc0d466028377b9374
danielblacha <daniel.blacha@gmail.com>
2018-09-30 15:13:58 +0200

    add style

--- src/main/java/pl/akademiakodu/generateCv/controller/PersonController.java
+++ src/main/java/pl/akademiakodu/generateCv/controller/PersonController.java
@@ -3,14 +3,22 @@ package pl.akademiakodu.generateCv.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import pl.akademiakodu.generateCv.model.Person;
 
 @Controller
 public class PersonController {
 
     @GetMapping("/")
-    public String showForm(ModelMap modelMap){
+    public String showForm(ModelMap modelMap) {
         modelMap.addAttribute("person", new Person());
         return "homePage";
+    }
+
+    @PostMapping("/cvform")
+    public String generateCV(@ModelAttribute Person person, ModelMap modelMap) {
+        modelMap.addAttribute("person", person);
+        return "generatedCV";
     }
 }
